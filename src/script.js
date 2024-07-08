6bd53a49da644529af8ea2b8e9d4efd0261d02fa
chanelbriggs24 <chanelbriggs24@gmail.com>
2024-07-08 14:35:25 -0400

    Added a function called getForecastData(city) to get the forecast data from the forecast API
    The getForecast function call will also happen at the end of the updateWeather function so that it is receiving the correct city retrieved from the api object

--- src/script.js
+++ src/script.js
@@ -57,6 +57,8 @@ function updateWeather(response) {
   cityElement.innerHTML = response.data.city;
   dayAndTime.innerHTML = formatDate(date);
   tempVal.innerHTML = temperature;
+
+  getForecastData(response.data.city);
 }
 
 function searchCity(city) {
@@ -80,37 +82,69 @@ function searchEvent(event) {
 let citySearch = document.querySelector(".search-form");
 citySearch.addEventListener("submit", searchEvent);
 
-function displayForecast() {
-  let daysOfWeek = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  // This code takes the array of daysOfWeek and it will loop through
-  //each element and perform the function below on each item in the array
+function getForecastData(city) {
+  let apiKey = `abf74f3d08ac0ba0527t801bd8o47a65`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
 
-  //This variable that has an empty string will store each element forecast
-  //after the loop performs the function on the day and then the next element will
-  //be concatenated to the string of variables instead of it being overwritten
-
-  let forecastHTML = ``;
-
-  daysOfWeek.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-  <div class="weather-forecast-day">
-    <div class="weather-forecast-weekday">${day}</div>
-    <div class="weather-forecast-icon">⛅</div>
-    <div class="weather-forecast-temp">
-      <div class="weather-forecast-tempRange">
-        <strong>15&deg;</strong>
-      </div>
-      <div class="weather-forecast-tempRange">9&deg;</div>
-    </div>
-  </div>
-`;
-  });
-  let forecast = document.querySelector("#forecast");
-  forecast.innerHTML = forecastHTML;
+  axios.get(apiUrl).then(displayForecast);
 }
 
-displayForecast();
+function displayForecast(response) {
+  console.log(response.data);
 
-searchCity("Charlotte");
+  let daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  //   // This code takes the array of daysOfWeek and it will loop through
+  //   //each element and perform the function below on each item in the array
+
+  //   //This variable that has an empty string will store each element forecast
+  //   //after the loop performs the function on the day and then the next element will
+  //   //be concatenated to the string of variables instead of it being overwritten
+  let forecastInfo = ``;
+
+  daysOfWeek.forEach(function (day) {
+    forecastInfo += `<div class="weather-forecast-day">
+     <div class="weather-forecast-weekday">${day}</div>
+     <div class="weather-forecast-icon">⛅</div>
+     <div class="weather-forecast-temp">
+       <div class="weather-forecast-tempRange">
+         <strong>15&deg;</strong>
+       </div>
+       <div class="weather-forecast-tempRange">9&deg;</div>
+     </div>
+   </div>
+ `;
+  });
+
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastInfo;
+}
+
+// function displayForecast() {
+//   let daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//   //   // This code takes the array of daysOfWeek and it will loop through
+//   //   //each element and perform the function below on each item in the array
+
+//   //   //This variable that has an empty string will store each element forecast
+//   //   //after the loop performs the function on the day and then the next element will
+//   //   //be concatenated to the string of variables instead of it being overwritten
+//   let forecastInfo = ``;
+
+//   daysOfWeek.forEach(function (day) {
+//     forecastInfo += `<div class="weather-forecast-day">
+//      <div class="weather-forecast-weekday">${day}</div>
+//      <div class="weather-forecast-icon">⛅</div>
+//      <div class="weather-forecast-temp">
+//        <div class="weather-forecast-tempRange">
+//          <strong>15&deg;</strong>
+//        </div>
+//        <div class="weather-forecast-tempRange">9&deg;</div>
+//      </div>
+//    </div>
+//  `;
+//   });
+
+//   let forecast = document.querySelector("#forecast");
+//   forecast.innerHTML = forecastInfo;
+// }
+
+searchCity("Paris");
